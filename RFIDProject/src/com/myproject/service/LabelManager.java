5d34f2ee90103b7597921bd7e6e3b543371f9be6
DESKTOP-I5JE1LC\XieYingjie <xyj222310@163.com>
2016-09-17 16:51:47 +0800

    rfid服务器代码

---
+++ RFIDProject/src/com/myproject/service/LabelManager.java
@@ -0,0 +1,103 @@
+package com.myproject.service;
+
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.myproject.DAO.LabelDAO;
+import com.myproject.DAOimpl.LabelDAOImpl;
+import com.myproject.Model.Factory;
+import com.myproject.Model.Store;
+import com.myproject.Model.Tags;
+import com.myproject.Model.Transport;
+
+/*
+ * 药品管理类：  作为服务器：主要方法是 一个响应请求的处理。即查询发送指定数据格式 的数据 给web或者app
+ * 作为 web程序，这个类是用dopost（）方法实现向服务器请求数据。
+ */
+
+public class LabelManager {
+	public LabelManager() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public JSONArray sendToWeb(String table,Double tagid){
+		JSONArray jArray = new JSONArray();
+		LabelDAO db = new LabelDAOImpl();
+		try{
+			switch (table) {
+			case "factory":
+				List fac;
+				if(tagid==0){
+					fac= db.queryAll("factory").list();
+				}
+				else fac= db.queryfromdb("factory",tagid).list();
+				for(int i=0;i<fac.size();i++){
+					JSONObject jso= new JSONObject();
+					jso.put("tagid", ((Factory) fac.get(i)).getTags().getTagsid());
+					jso.put("factoryname", ((Factory) fac.get(i)).getFactoryname());
+					jso.put("leavestatus", ((Factory) fac.get(i)).getLeaveFactoryStatus());
+					jso.put("outputtime", ((Factory) fac.get(i)).getOutputtime());
+					jso.put("line1status", ((Factory) fac.get(i)).getProductLine1Status());
+					jso.put("line2status", ((Factory) fac.get(i)).getProductLine2Stauts());
+					jArray.put(i, jso);
+				}
+				break;
+			case "store":
+				List store;
+				if(tagid==0){
+					store= db.queryAll("store").list();
+				}
+				else store= db.queryfromdb("store",tagid).list();
+				for(int i=0;i<store.size();i++){
+					JSONObject jso= new JSONObject();
+					jso.put("tagid", ((Store) store.get(i)).getTags().getTagsid());
+					jso.put("storename", ((Store) store.get(i)).getStorename());
+					jso.put("stocktime", ((Store) store.get(i)).getStocktime());
+					jso.put("soldtime", ((Store) store.get(i)).getSoldtime());
+					jso.put("storestatus", ((Store) store.get(i)).getStorestatus());
+					jArray.put(i, jso);
+				}
+				break;
+			case "tag":
+				List tag;
+				if(tagid==0){
+					tag= db.queryAll("tag").list();
+				}
+				else tag= db.queryfromdb("tag",tagid).list();
+				for(int i=0;i<tag.size();i++){
+					JSONObject jso= new JSONObject();
+					jso.put("tagid", ((Tags) tag.get(i)).getTagsid());
+					jso.put("medicine", ((Tags) tag.get(i)).getMedicinename());
+					jArray.put(i, jso);
+				}
+				break;
+			case "transport":
+				List trans;
+				if(tagid==0){
+					trans= db.queryAll("transport").list();
+				}
+				else trans= db.queryfromdb("transport",tagid).list();
+				for(int i=0;i<trans.size();i++){
+					JSONObject jso= new JSONObject();
+					jso.put("tagid", ((Transport) trans.get(i)).getTags().getTagsid());
+					jso.put("transportname", ((Transport) trans.get(i)).getTansportname());
+					jso.put("starttime", ((Transport) trans.get(i)).getStarttime());
+					jso.put("endtime", ((Transport) trans.get(i)).getEndtime());
+					jso.put("transportstatus", ((Transport) trans.get(i)).getTransportstatus());
+					jArray.put(i, jso);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return jArray;
+	}
+	
+}
